0317b7015de72cf69956a49977e41de10e25d5ea
Alec Hunter <alecryanhunter@gmail.com>
2023-05-02 17:40:13 -0700

    created SQL schema

--- README.md
+++ README.md
@@ -6,4 +6,11 @@
 
 ## Installation
 
+1- Clone the repository
+1-2- Navigate to the Employee-Tracker folder in your terminal
+2- Run the npm i command
+3- Login to MySql using the command "mysql -uUSER -pPASSWORD", replacing USER and PASSWORD with your relevant credentials
+4- In the SQL command line, run the commands SOURCE db/schema.sql; and SOURCE db/seeds.sql; in that order, then exit your SQL command line
+5- Run the node index.js command to start the program
+
 ## Credits
--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,34 @@
-DROP DATABASE IF EXISTS 
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE NO ACTION,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+)
